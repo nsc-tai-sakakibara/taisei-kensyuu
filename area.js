0714ebcbcf2a9b68482c620d6d2b3cbf928666d3
nsc-tai-sakakibara <63280654+nsc-tai-sakakibara@users.noreply.github.com>
2020-04-09 19:18:44 +0900

    初jabascript

---
+++ area.js
@@ -0,0 +1,34 @@
+/*
+ * 四角の面積を計算してください。
+ *
+ * length: 縦の長さ
+ * width: 横の長さ
+ */
+
+function getArea(length, width) {
+    let area;
+
+    area = length * width;
+    return area;
+    console.log(area);
+
+}
+getArea(2,3);
+console.log(area);
+
+/**
+ * 四角の周囲を計算してください。
+ *
+ *
+ * length: 縦の長さ
+ * width: 横の長さ
+ */
+function getPerimeter(length, width) {
+    let perimeter
+
+    perimeter = length * 2 + width * 2;
+    return perimeter
+    console.log(perimeter);
+
+}
+getPerimeter(2,3);
